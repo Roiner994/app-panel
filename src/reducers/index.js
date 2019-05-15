392a9a0281c5eb9681d89dedd90f43f64747702f
Roiner Hernandez <roiner123@gmail.com>
2019-05-14 21:59:50 -0400

    panel react sin dashboard

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+import { combineReducers } from 'redux';
+import { users } from './users';
+import { authentication } from './authentication';
+import { categories } from './categories';
+import { products } from './products';
+import { paths } from './paths';
+import { reducer as reduxForm } from "redux-form";
+
+export default combineReducers ({
+    users,
+    form: reduxForm,
+    authentication,
+    categories,
+    products,
+    paths,
+});
